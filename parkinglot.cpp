df8b4b74acd094a0c4bb632bcd0a99aa311d0457
Ritwika Pal <60825125+Ritwika101@users.noreply.github.com>
2023-12-08 05:47:42 +0530

    added comments

--- parkinglot.cpp
+++ parkinglot.cpp
@@ -1,3 +1,13 @@
+/*
+This is the object oriented solution of a parking lot with multiple floors, and multiple spots. 
+Keeping it simple, 3 vehicle types can be present - bike, car, truck
+3 spots can be assigned respectively to them - motorcycle, compact, large.
+Ideally while calculation of the ending time, that is the time of the exit of the vehicle, the current time should be fetched, 
+but since this code executes very quickly, I have take an hour difference of 5 for every exit.
+The main function has the implmentation of one of the possible flows of the system.
+Used design patterns : Factory pattern and Singleton
+*/
+
 #include<iostream>
 #include <vector>
 #include <unordered_map>
